57870222482d434975a4d6eeb0e44aa12bb48925
Falesz <jnjwrg@inf.elte.hu>
2024-08-07 20:23:06 +0200

    Added event listener for keydown event

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const table = document.getElementById("guesses-table");
+let secretWord = "paddy";
+let currentGuess = "";
+let guessCount = 1;
 
 function initTable() {
-    for (let i = 0; i < 7; ++i) {
+    for (let i = 0; i < 6; ++i) {
         table.innerHTML += `<tr id=\"guess-row${i+1}\"></tr>`;
         let currentRow = document.getElementById("guess-row" + (i+1));
-        for (let j = 0; j < 6; ++j) {
+        for (let j = 0; j < 5; ++j) {
             currentRow.innerHTML += "<td></td>";
         }
     }
 }
 
 initTable();
+
+document.addEventListener("keydown", (event) => { console.log(event.code); });
