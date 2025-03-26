7252c734d9373a2f0247edbd1c3e11ca8eafa35b
Falesz <jnjwrg@inf.elte.hu>
2025-03-26 23:31:58 +0100

    Update script.js
    Added functionality to clear the contents of the table and fill it's first row with the first guess

--- script.js
+++ script.js
@@ -13,6 +13,36 @@ function initTable() {
     }
 }
 
+function clearTable() {
+    [...table.querySelectorAll("tr")].forEach(row => {
+        [...row.children].forEach(cell => {
+            cell.innerText = "";
+        })
+    });
+}
+
+function updateTableContents() {
+    console.log(currentGuess);
+    clearTable();
+    let currentRow = document.getElementById(`guess-row${guessCount}`);
+    for (let i = 0; i < currentGuess.length; ++i) {
+        currentRow.children[i].innerText = currentGuess.charAt(i);
+    }
+}
+
 initTable();
 
-document.addEventListener("keydown", (event) => { console.log(event.code); });
+document.addEventListener("keydown", (event) => {
+    if (event.code === "Backspace" && currentGuess.length > 0) {
+        currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+        updateTableContents();
+        return;
+    }
+
+    if (event.code !== `Key${event.key.toUpperCase()}`) return;
+
+    if (currentGuess.length < 5) {
+        currentGuess += event.key;
+    }
+    updateTableContents();
+});
